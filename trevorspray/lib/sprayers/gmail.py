83c4ce6aa2c00f50ea3222043f757ac52ac5ef8b
ctechs1 <140086509+ctechs1@users.noreply.github.com>
2023-07-28 15:27:03 +0100

    Update gmail.py

--- trevorspray/lib/sprayers/gmail.py
+++ trevorspray/lib/sprayers/gmail.py
@@ -11,7 +11,7 @@ class SprayModule(BaseSprayModule):
     # HTTP method
     method = 'POST'
     # default target URL
-    default_url = 'https://accounts.google.com/v3/signin/challenge/pwd?TL=AJvNCbar5qtMxhAinFsXoLOvo3TbrkISaStxTeN_9cechXj5g84zrkTLHnlTHDyk&checkConnection=youtube%3A273%3A0&checkedDomains=youtube&cid=1&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F%26emr%3D1%26ltmpl%3Ddefault%26ltmplcache%3D2%26osid%3D1%26passive%3Dtrue%26password%3Dmybadpassword%26rm%3Dfalse%26scc%3D1%26service%3Dmail%26ss%3D1%26username%3Dctechs123%40gmail.com%26ifkv%3DAeDOFXhmC-wYkGSUmBaMWvgQzNdNC51wRnAaEck5Gi69RLmNhzrap_DoW1DUZs-IVHPz-7fTOG0uuQ%2F&dsh=S-83148827%3A1690554033508855&emr=1&flowEntry=ServiceLogin&flowName=GlifWebSignIn&ifkv=AeDOFXg6jc-OZf1JMYxw3jh9jpb2f4x78eKUgEJpOVx291yiYcCwyA62jzhTxgnMUKLFqEBi3ZXHVw&ltmpl=default&osid=1&pstMsg=1&rm=false&scc=1&service=mail&ss=1'
+    default_url = 'https://accounts.google.com/v3/signin/identifier?'
     # body of request
     request_data = { 
 		  "username": "{username}",
